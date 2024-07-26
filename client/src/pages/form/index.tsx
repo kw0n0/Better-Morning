0760858a7e75fabbce1c2dd0408955387693ad23
kw0n0 <alwayskw0n0@gmail.com>
2024-07-26 17:14:47 +0900

    feat: mock 데이터로 테스트

--- client/src/pages/form/index.tsx
+++ client/src/pages/form/index.tsx
@@ -1,9 +1,14 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import TextInput from '../../common/components/TextInput';
+import TimeInput from '../../common/components/TimeInput';
 
 function Form() {
-  const SERVER_DATA = [{ id: 0, type: 'text', title: '취침 30분전 행동?' }];
+  const SERVER_DATA = [
+    { id: 0, type: 'text', title: '취침 30분전 행동?' },
+    { id: 1, type: 'time', title: '취침 시간', defaultValue: '23:00' },
+    { id: 2, type: 'time', title: '기상 시간', defaultValue: '06:00' },
+  ];
 
   return (
     <form
@@ -23,6 +28,19 @@ function Form() {
               height={30}
               placeholder={'텍스트를 입력해주세요'}
               title={item.title}
+            />
+          );
+        }
+
+        if (item.type === 'time') {
+          return (
+            <TimeInput
+              key={item.id}
+              id={`${item.id}`}
+              width={400}
+              height={30}
+              title={item.title}
+              defaultValue={item.defaultValue}
             />
           );
         }
