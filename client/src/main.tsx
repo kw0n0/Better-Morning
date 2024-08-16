2163116cae4f5ab056d609335a8b37fd2184b128
kw0n0 <alwayskw0n0@gmail.com>
2024-08-17 00:00:10 +0900

    [client] feat: router-dom 및 my페이지 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './pages/home/index';
-import Form from './pages/form/index';
-import './reset.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import Login from './pages/login';
 import Layout from './pages/layout';
+import Form from './pages/form';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/form',
-    element: <Form />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-]);
+import './reset.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthProvider';
+import My from './pages/my';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Layout>
-      <RouterProvider router={router} />
-    </Layout>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/my" element={<My />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ client/src/pages/my/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function My() {
+  return (
+    <>
+      <Link to="/my">마이페이지</Link>
+    </>
+  );
+}
+
+export default My;
